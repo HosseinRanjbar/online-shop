34de92a3068442e2e52eb09aa2e57f2f373db03a
Sahab <amirisahab@gmail.com>
2024-06-04 00:18:56 +0330

    product

---
+++ src/Components/Product.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Product = ({ products }) => {
+  console.log(products);
+  return (
+    <div>
+      <div className="border h-80 relative flex flex-col justify-center items-center group transition">
+        <img
+          className="max-h-40 group-hover:scale-110 transition duration-300"
+          src={products.image}
+        />
+        <div className="absolute top-3 -right-11 group-hover:right-5 p-2 flex gap-y-2 flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-all duration-300">
+            <button className="text-white text-3xl bg-red-500 p-2 w-12 h-12 flex justify-center items-center">+</button>
+            <Link to={`/product/${products.id}`} className="w-12 h-12 shadow-md bg-white flex justify-center items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="size-7"
+            >
+              <path d="M10 12.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
+              <path
+                fillRule="evenodd"
+                d="M.664 10.59a1.651 1.651 0 0 1 0-1.186A10.004 10.004 0 0 1 10 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0 1 10 17c-4.257 0-7.893-2.66-9.336-6.41ZM14 10a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            </Link>
+        </div>
+      </div>
+      <div>
+        <div className="capitalize text-gray-500 text-sm">{products.category}</div>
+        <Link to={`/product/${products.id}`}><h2 className="font-semibold">{products.title}</h2></Link>
+        <div className="font-semibold">${products.price}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,28 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { ProductContext } from "../Context/ProductProvider";
+import Product from "../Components/Product";
 const Home = () => {
-  return <div>Home</div>;
+  const { products } = useContext(ProductContext);
+  const filteredProducts = products.filter((item) => {
+    return item.category === "women's clothing";
+  });
+  console.log(filteredProducts);
+
+  return (
+    <div>
+      <section>
+        <div className="container mx-auto flex flex-col items-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm md:max-w-none md:mx">
+            {filteredProducts.map((products) => (
+              <div className="w-full " key={products.id}>
+                {<Product products={products} />}
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
