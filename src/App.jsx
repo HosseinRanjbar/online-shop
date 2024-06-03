86658e50a44c5681c2b0874f270eb30b76ebcc6a
Sahab <amirisahab@gmail.com>
2024-06-03 17:49:43 +0330

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import ProductDetails from "./Pages/ProductDetails";
+import Header from "./Components/Header";
+import SideBar from "./Components/SideBar";
+import Footer from "./Components/Footer";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+        </Routes>
+        <SideBar />
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
